e89d38e0014bac29646dc99b4211f1ccddf1a203
andre <minukee@ino-on.com>
2016-11-29 18:23:51 +0900

    TEST-1 admin page manual check

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class SensorManagement(models.Model):
+    manager = models.ForeignKey('auth.User')
+    userId = models.AutoField(primary_key=True)
+    nodeId = models.IntegerField(max_length=10,null=False)
+    Latitude = models.FloatField(max_length=20)
+    Longitude = models.FloatField(max_length=20)
+    Address = models.CharField(max_length=100)
+    Type = models.CharField(max_length=10)
+    img = models.ImageField(upload_to='pic_folder/', default='pic_folder/unknown_icon.png')
